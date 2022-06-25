d2ced26d7f7bcd381a30dfbd99526e02bb04ae58
To Huynh <huynh.nto@gmail.com>
2022-06-25 09:16:42 -0700

    Add seo

--- components/page/index.tsx
+++ components/page/index.tsx
@@ -14,14 +14,35 @@ type PageProps = {
   children: ReactNode
 }
 
-/**TODO: add SEO */
-
 const Page: FC<PageProps> = ({ children }) => {
   return (
     <>
       <Head>
         <title>To Huynh</title>
+        <meta name="title" content="To Huynh" />
+        <meta
+          name="description"
+          content="Self-taught software developer."
+        />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta charSet="UTF-8" />
+        
+        <meta property="og:title" content="To Huynh" />
+        <meta
+          property="og:description"
+          content="Self-taught software developer."
+        />
+        <meta property="og:url" content="https://tohuynh.github.io" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:title" content="To Huynh" />
+        <meta
+          name="twitter:description"
+          content="Self-taught software developer."
+        />
+        <meta name="twitter:url" content="https://tohuynh.github.io" />
+        <meta name="twitter:card" content="summary" />
       </Head>
       <ThemeProvider theme={darkTheme}>
         <Paper sx={{ minHeight: "100vh", width: "100%" }} square={true}>
