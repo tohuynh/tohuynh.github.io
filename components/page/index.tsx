ae6ceb9dc643fc174027e9b4d04040f32d57063e
To Huynh <huynh.nto@gmail.com>
2022-06-25 20:42:21 -0700

    Add favicon

--- components/page/index.tsx
+++ components/page/index.tsx
@@ -25,6 +25,12 @@ const Page: FC<PageProps> = ({ children, style }) => {
         <meta name="twitter:description" content="Self-taught software developer." />
         <meta name="twitter:url" content="https://tohuynh.github.io" />
         <meta name="twitter:card" content="summary" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Paper
         sx={{ minHeight: "100vh", width: "100%", paddingBottom: "50px" }}
