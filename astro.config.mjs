deed97781a7fc83c002f211afa69d323738e9b28
To Huynh <huynh.nto@gmail.com>
2023-05-03 11:43:14 -0700

    Try party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
-
+import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: "https://tohuynh.github.io",
 });
