506b736b554af7cea6bbee74ca555d3d60865261
To Huynh <huynh.nto@gmail.com>
2023-07-01 22:19:28 -0700

    mdx int

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,46 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import remarkCodeTitles from "remark-code-titles";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noreferrer noopener"],
+          content: {
+            type: "text",
+            value: "↗",
+          },
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: "heading-link heading-link--hidden---effects",
+            "data-heading-link": true,
+          },
+          behavior: "wrap",
+        },
+      ],
+    ],
+  },
   integrations: [
     tailwind(),
     partytown({
@@ -12,6 +49,7 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    mdx(),
   ],
   site: "https://tohuynh.github.io",
 });
